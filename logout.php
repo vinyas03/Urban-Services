7a5cd83d69c0b20f8f0bb70e489cbb0ce4795739
Vinyas <vinyas2345@gmail.com>
2023-11-28 01:35:11 +0530

    minor tweak: logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
 session_start();
-if(isset($_SESSION['user_id'])) {
+if(isset($_SESSION['userID'])) {
+	//unset
+
 	session_destroy();
-	unset($_SESSION['user_id']);
-	unset($_SESSION['user_name']);
 	header("Location: index.php");
 } else {
 	header("Location: index.php");
