8b07d69050da17cddbdc6481013f4ecde1f1a387
Vinyas <vinyas2345@gmail.com>
2023-12-08 19:22:08 +0530

    Added AOS animations in landing page

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ include_once("db_connect.php");
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap" rel="stylesheet">
-    
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <title>Urban Services - Home</title>
 </head>
 <body>
@@ -23,13 +24,13 @@ include_once("db_connect.php");
             <img class="menu" src="./images/svg/burger-menu-left.svg" width="40px" height="40px">
         </div>
         <div class="logo-wrapper">
-            <img src="./images/png/logo.png" class="logo">
-            <div class="title">Urban Services</div>
+            <img src="./images/png/logo.jpeg" class="logo" />
+            <!-- <div class="title">Urban Services</div> -->
         </div>
         <ul class="nav-items">
             <li><a href="./index.php">Home</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Register a service</a></li>
+            <li><a href="./registerservice.php">Register a service</a></li>
         </ul>
         <div class="nav-buttons">
         <?php if (isset($_SESSION['user_id'])) { ?>
@@ -62,17 +63,21 @@ include_once("db_connect.php");
 
     <div class="hero-container">
         <div class="hero-text">
-            <h1 class="hero-title">Urban Services</h1>
+            <h2 class="hero-title" data-aos="fade-right" data-aos-duration="1000"
+    data-aos-easing="ease-in-out">Hire Experts & Get Your Job Done</h2>
             <p class="hero-info">
-                Your one stop destination for Home Services.
-                We provide services like: Plumbing, Carpentry, Cleaning and Electrical works.
+                
             </p>
         </div>
         <div class="hero-image">
-            <img src="./images/png/mechanic.png" alt="hero image">
+            <img src="./images/png/man.png" alt="hero image" data-aos="fade-left" data-aos-duration="1000"
+    data-aos-easing="ease-in-out">
         </div>
     </div>
 
     <script src="./scripts/script.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
